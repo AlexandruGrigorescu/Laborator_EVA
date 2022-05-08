c8419477b14f80a21ead9111e10ed006482c10a8
AlexandruGrigorescu <alexandru.andrei.grigorescu@gmail.com>
2022-05-08 21:05:30 +0300

    Laboratoare 9&10

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,332 @@
+﻿namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadImage = new System.Windows.Forms.Button();
+            this.originalPicture = new System.Windows.Forms.PictureBox();
+            this.modifiedPicture = new System.Windows.Forms.PictureBox();
+            this.btnConvertToGrayscale = new System.Windows.Forms.Button();
+            this.btnGenerateHistogram = new System.Windows.Forms.Button();
+            this.alphaTextBox = new System.Windows.Forms.TextBox();
+            this.betaTextBox = new System.Windows.Forms.TextBox();
+            this.alfaLabel = new System.Windows.Forms.Label();
+            this.betaLabel = new System.Windows.Forms.Label();
+            this.btnChangeContrast = new System.Windows.Forms.Button();
+            this.gammaTextBox = new System.Windows.Forms.TextBox();
+            this.gammaLabel = new System.Windows.Forms.Label();
+            this.btnGammaCorrection = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.btnRotate = new System.Windows.Forms.Button();
+            this.btnLoadVideo = new System.Windows.Forms.Button();
+            this.frameCounter = new System.Windows.Forms.NumericUpDown();
+            this.btnStartVideo = new System.Windows.Forms.Button();
+            this.framesLabel = new System.Windows.Forms.Label();
+            this.btnBlendImages = new System.Windows.Forms.Button();
+            this.btnWriteLogoToVideo = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.originalPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.modifiedPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameCounter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoadImage
+            // 
+            this.btnLoadImage.Location = new System.Drawing.Point(12, 12);
+            this.btnLoadImage.Name = "btnLoadImage";
+            this.btnLoadImage.Size = new System.Drawing.Size(114, 43);
+            this.btnLoadImage.TabIndex = 0;
+            this.btnLoadImage.Text = "Load image";
+            this.btnLoadImage.UseVisualStyleBackColor = true;
+            this.btnLoadImage.Click += new System.EventHandler(this.LoadImage_Click);
+            // 
+            // originalPicture
+            // 
+            this.originalPicture.Location = new System.Drawing.Point(134, 119);
+            this.originalPicture.Name = "originalPicture";
+            this.originalPicture.Size = new System.Drawing.Size(444, 354);
+            this.originalPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.originalPicture.TabIndex = 1;
+            this.originalPicture.TabStop = false;
+            this.originalPicture.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.originalPicture.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.originalPicture.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.originalPicture.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // modifiedPicture
+            // 
+            this.modifiedPicture.Location = new System.Drawing.Point(584, 119);
+            this.modifiedPicture.Name = "modifiedPicture";
+            this.modifiedPicture.Size = new System.Drawing.Size(449, 354);
+            this.modifiedPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.modifiedPicture.TabIndex = 2;
+            this.modifiedPicture.TabStop = false;
+            // 
+            // btnConvertToGrayscale
+            // 
+            this.btnConvertToGrayscale.Location = new System.Drawing.Point(132, 12);
+            this.btnConvertToGrayscale.Name = "btnConvertToGrayscale";
+            this.btnConvertToGrayscale.Size = new System.Drawing.Size(116, 42);
+            this.btnConvertToGrayscale.TabIndex = 3;
+            this.btnConvertToGrayscale.Text = "Convert to grayscale";
+            this.btnConvertToGrayscale.UseVisualStyleBackColor = true;
+            this.btnConvertToGrayscale.Click += new System.EventHandler(this.ConvertToGrayscale_Click);
+            // 
+            // btnGenerateHistogram
+            // 
+            this.btnGenerateHistogram.Location = new System.Drawing.Point(254, 12);
+            this.btnGenerateHistogram.Name = "btnGenerateHistogram";
+            this.btnGenerateHistogram.Size = new System.Drawing.Size(114, 42);
+            this.btnGenerateHistogram.TabIndex = 4;
+            this.btnGenerateHistogram.Text = "Generate histogram";
+            this.btnGenerateHistogram.UseVisualStyleBackColor = true;
+            this.btnGenerateHistogram.Click += new System.EventHandler(this.GenerateHistogram_Click);
+            // 
+            // alphaTextBox
+            // 
+            this.alphaTextBox.Location = new System.Drawing.Point(422, 70);
+            this.alphaTextBox.Name = "alphaTextBox";
+            this.alphaTextBox.Size = new System.Drawing.Size(53, 22);
+            this.alphaTextBox.TabIndex = 5;
+            // 
+            // betaTextBox
+            // 
+            this.betaTextBox.Location = new System.Drawing.Point(545, 70);
+            this.betaTextBox.Name = "betaTextBox";
+            this.betaTextBox.Size = new System.Drawing.Size(53, 22);
+            this.betaTextBox.TabIndex = 6;
+            // 
+            // alfaLabel
+            // 
+            this.alfaLabel.AutoSize = true;
+            this.alfaLabel.Location = new System.Drawing.Point(386, 73);
+            this.alfaLabel.Name = "alfaLabel";
+            this.alfaLabel.Size = new System.Drawing.Size(30, 16);
+            this.alfaLabel.TabIndex = 7;
+            this.alfaLabel.Text = "Alfa";
+            // 
+            // betaLabel
+            // 
+            this.betaLabel.AutoSize = true;
+            this.betaLabel.Location = new System.Drawing.Point(504, 74);
+            this.betaLabel.Name = "betaLabel";
+            this.betaLabel.Size = new System.Drawing.Size(35, 16);
+            this.betaLabel.TabIndex = 8;
+            this.betaLabel.Text = "Beta";
+            // 
+            // btnChangeContrast
+            // 
+            this.btnChangeContrast.Location = new System.Drawing.Point(374, 13);
+            this.btnChangeContrast.Name = "btnChangeContrast";
+            this.btnChangeContrast.Size = new System.Drawing.Size(114, 43);
+            this.btnChangeContrast.TabIndex = 9;
+            this.btnChangeContrast.Text = "Change contrast";
+            this.btnChangeContrast.UseVisualStyleBackColor = true;
+            this.btnChangeContrast.Click += new System.EventHandler(this.ChangeContrast_Click);
+            // 
+            // gammaTextBox
+            // 
+            this.gammaTextBox.Location = new System.Drawing.Point(675, 70);
+            this.gammaTextBox.Name = "gammaTextBox";
+            this.gammaTextBox.Size = new System.Drawing.Size(53, 22);
+            this.gammaTextBox.TabIndex = 11;
+            // 
+            // gammaLabel
+            // 
+            this.gammaLabel.AutoSize = true;
+            this.gammaLabel.Location = new System.Drawing.Point(614, 73);
+            this.gammaLabel.Name = "gammaLabel";
+            this.gammaLabel.Size = new System.Drawing.Size(55, 16);
+            this.gammaLabel.TabIndex = 12;
+            this.gammaLabel.Text = "Gamma";
+            // 
+            // btnGammaCorrection
+            // 
+            this.btnGammaCorrection.Location = new System.Drawing.Point(494, 14);
+            this.btnGammaCorrection.Name = "btnGammaCorrection";
+            this.btnGammaCorrection.Size = new System.Drawing.Size(114, 42);
+            this.btnGammaCorrection.TabIndex = 13;
+            this.btnGammaCorrection.Text = "Gamma correction";
+            this.btnGammaCorrection.UseVisualStyleBackColor = true;
+            this.btnGammaCorrection.Click += new System.EventHandler(this.GammaCorrection_Click);
+            // 
+            // btnResize
+            // 
+            this.btnResize.Location = new System.Drawing.Point(614, 14);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(114, 42);
+            this.btnResize.TabIndex = 14;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.ResizeImage_Click);
+            // 
+            // btnRotate
+            // 
+            this.btnRotate.Location = new System.Drawing.Point(734, 14);
+            this.btnRotate.Name = "btnRotate";
+            this.btnRotate.Size = new System.Drawing.Size(114, 42);
+            this.btnRotate.TabIndex = 15;
+            this.btnRotate.Text = "Rotate";
+            this.btnRotate.UseVisualStyleBackColor = true;
+            this.btnRotate.Click += new System.EventHandler(this.RotateImage_Click);
+            // 
+            // btnLoadVideo
+            // 
+            this.btnLoadVideo.Location = new System.Drawing.Point(12, 61);
+            this.btnLoadVideo.Name = "btnLoadVideo";
+            this.btnLoadVideo.Size = new System.Drawing.Size(114, 42);
+            this.btnLoadVideo.TabIndex = 16;
+            this.btnLoadVideo.Text = "Load video";
+            this.btnLoadVideo.UseVisualStyleBackColor = true;
+            this.btnLoadVideo.Click += new System.EventHandler(this.LoadVideo_Click);
+            // 
+            // frameCounter
+            // 
+            this.frameCounter.Location = new System.Drawing.Point(135, 507);
+            this.frameCounter.Name = "frameCounter";
+            this.frameCounter.Size = new System.Drawing.Size(113, 22);
+            this.frameCounter.TabIndex = 17;
+            // 
+            // btnStartVideo
+            // 
+            this.btnStartVideo.Location = new System.Drawing.Point(132, 60);
+            this.btnStartVideo.Name = "btnStartVideo";
+            this.btnStartVideo.Size = new System.Drawing.Size(116, 43);
+            this.btnStartVideo.TabIndex = 18;
+            this.btnStartVideo.Text = "Start video";
+            this.btnStartVideo.UseVisualStyleBackColor = true;
+            this.btnStartVideo.Click += new System.EventHandler(this.StartVideo_Click);
+            // 
+            // framesLabel
+            // 
+            this.framesLabel.AutoSize = true;
+            this.framesLabel.Location = new System.Drawing.Point(131, 488);
+            this.framesLabel.Name = "framesLabel";
+            this.framesLabel.Size = new System.Drawing.Size(53, 16);
+            this.framesLabel.TabIndex = 19;
+            this.framesLabel.Text = "Frames";
+            // 
+            // btnBlendImages
+            // 
+            this.btnBlendImages.Location = new System.Drawing.Point(854, 14);
+            this.btnBlendImages.Name = "btnBlendImages";
+            this.btnBlendImages.Size = new System.Drawing.Size(114, 41);
+            this.btnBlendImages.TabIndex = 20;
+            this.btnBlendImages.Text = "Blend images";
+            this.btnBlendImages.UseVisualStyleBackColor = true;
+            this.btnBlendImages.Click += new System.EventHandler(this.BlendImages_Click);
+            // 
+            // btnWriteLogoToVideo
+            // 
+            this.btnWriteLogoToVideo.Location = new System.Drawing.Point(254, 60);
+            this.btnWriteLogoToVideo.Name = "btnWriteLogoToVideo";
+            this.btnWriteLogoToVideo.Size = new System.Drawing.Size(114, 43);
+            this.btnWriteLogoToVideo.TabIndex = 21;
+            this.btnWriteLogoToVideo.Text = "Write to video";
+            this.btnWriteLogoToVideo.UseVisualStyleBackColor = true;
+            this.btnWriteLogoToVideo.Click += new System.EventHandler(this.WriteLogoToVideo_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Abrupt video play";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 584);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnWriteLogoToVideo);
+            this.Controls.Add(this.btnBlendImages);
+            this.Controls.Add(this.framesLabel);
+            this.Controls.Add(this.btnStartVideo);
+            this.Controls.Add(this.frameCounter);
+            this.Controls.Add(this.btnLoadVideo);
+            this.Controls.Add(this.btnRotate);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.btnGammaCorrection);
+            this.Controls.Add(this.gammaLabel);
+            this.Controls.Add(this.gammaTextBox);
+            this.Controls.Add(this.btnChangeContrast);
+            this.Controls.Add(this.betaLabel);
+            this.Controls.Add(this.alfaLabel);
+            this.Controls.Add(this.betaTextBox);
+            this.Controls.Add(this.alphaTextBox);
+            this.Controls.Add(this.btnGenerateHistogram);
+            this.Controls.Add(this.btnConvertToGrayscale);
+            this.Controls.Add(this.modifiedPicture);
+            this.Controls.Add(this.originalPicture);
+            this.Controls.Add(this.btnLoadImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.originalPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.modifiedPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.frameCounter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadImage;
+        private System.Windows.Forms.Button btnConvertToGrayscale;
+        private System.Windows.Forms.Button btnGenerateHistogram;
+        private System.Windows.Forms.Button btnChangeContrast;
+        private System.Windows.Forms.Button btnGammaCorrection;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Button btnRotate;
+        private System.Windows.Forms.Button btnLoadVideo;
+        private System.Windows.Forms.Button btnStartVideo;
+        private System.Windows.Forms.Button btnBlendImages;
+        private System.Windows.Forms.Button btnWriteLogoToVideo;
+
+        private System.Windows.Forms.PictureBox originalPicture;
+        private System.Windows.Forms.PictureBox modifiedPicture;
+        
+        private System.Windows.Forms.TextBox alphaTextBox;
+        private System.Windows.Forms.TextBox betaTextBox;
+        private System.Windows.Forms.TextBox gammaTextBox;
+
+        private System.Windows.Forms.Label alfaLabel;
+        private System.Windows.Forms.Label betaLabel;
+        private System.Windows.Forms.Label gammaLabel;
+        private System.Windows.Forms.Label framesLabel;
+
+        private System.Windows.Forms.NumericUpDown frameCounter;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,219 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Operations.Class1;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+        Image<Bgr, byte> myImage;
+        Image<Bgr, byte> resizedImage;
+        Image<Bgr, byte> rotatedImage;
+        Image<Bgr, byte> gammaOutput;
+        Image<Gray, byte> grayImage;
+        Image<Gray, byte> imgOutput;
+        List<Image<Bgr, byte>> listImages;
+        Rectangle rectangle;
+        Point startROI;
+        bool mouseDown;
+
+        int TotalFrame, FrameNo;
+        double Fps;
+        bool IsReadingFrame;
+        VideoCapture capture;
+
+        //private static VideoCapture cameraCapture;
+        //private Image<Bgr, Byte> newBackgroundImage;
+        //private static IBackgroundSubtractor fgDetector;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void LoadImage_Click(object sender, EventArgs e)
+        {
+            modifiedPicture.Image = null;
+            LoadImage(ref myImage);
+            originalPicture.Image = myImage.ToBitmap();
+        }
+
+        private void ConvertToGrayscale_Click(object sender, EventArgs e)
+        {
+            ConvertToGrayscale(myImage, ref grayImage);
+            modifiedPicture.Image = grayImage.AsBitmap();
+        }
+
+        private void GenerateHistogram_Click(object sender, EventArgs e)
+        {
+            GenerateHistogram(myImage);
+        }
+
+        private void ChangeContrast_Click(object sender, EventArgs e)
+        {
+            float alpha = float.Parse(alphaTextBox.Text);
+            float beta = float.Parse(betaTextBox.Text);
+            ConvertToGrayscale(myImage, ref imgOutput);
+            ChangeContrast(alpha,beta,ref imgOutput);
+            modifiedPicture.Image = imgOutput.AsBitmap();
+        }
+
+        private void GammaCorrection_Click(object sender, EventArgs e)
+        {
+            double gamma = double.Parse(gammaTextBox.Text);
+            gammaOutput = myImage;
+            gammaOutput._GammaCorrect(gamma);
+            modifiedPicture.Image = gammaOutput.AsBitmap();
+        }
+
+        private void ResizeImage_Click(object sender, EventArgs e)
+        {
+            ResizeImage(myImage, ref resizedImage);
+            modifiedPicture.Image = resizedImage.AsBitmap();
+        }
+   
+        private void RotateImage_Click(object sender, EventArgs e)
+        {
+            RotateImage(myImage, ref rotatedImage);
+            modifiedPicture.Image = rotatedImage.AsBitmap();
+        }
+
+        private async void BlendImages_Click(object sender, EventArgs e)
+        {
+            BlendImages(ref listImages);
+            for (int i = 0; i < listImages.Count - 1; i++)
+            {
+                for (double alpha = 0.0; alpha <= 1.0; alpha += 0.01)
+                {
+                    modifiedPicture.Image = listImages[i + 1].AddWeighted(listImages[i], alpha, 1 - alpha, 0).AsBitmap();
+                    await Task.Delay(25);
+                }
+            }
+        }
+
+        private void LoadVideo_Click(object sender, EventArgs e)
+        {
+            modifiedPicture.Image = null;
+            string videoPath = @"C:\Users\Alex\Documents\GitHub\Laborator_EVA\Star Wars Reflections 4K Unreal Engine Real-Time Ray Tracing Demonstration (360).mp4";
+            Image output = originalPicture.Image;
+            LoadVideo(ref TotalFrame, ref FrameNo, ref Fps, ref capture, ref output, videoPath);
+            originalPicture.Image = output;
+            frameCounter.Minimum = 0;
+            frameCounter.Maximum = TotalFrame;
+            frameCounter.Value = FrameNo;
+        }
+
+        private void StartVideo_Click(object sender, EventArgs e)
+        {
+            if (capture == null) return;
+            IsReadingFrame = true;
+            ReadAllFrames();
+        }        
+
+        private void WriteLogoToVideo_Click(object sender, EventArgs e)
+        {
+            string videoPath = @"C:\Users\Alex\Documents\GitHub\Laborator_EVA\Do. Or do not. There is no try.mpg";
+            string destinationPath = @"C:\Users\Alex\Documents\GitHub\Laborator_EVA\ouput.mpg";
+            string logoPath = @"C:\Users\Alex\Documents\GitHub\Laborator_EVA\logo.jpg";
+
+            PutLogo(videoPath,destinationPath,logoPath);
+        }
+
+        private async void ReadAllFrames()
+        {
+            Mat m = new Mat();
+            while (IsReadingFrame == true && FrameNo < TotalFrame-1)
+            {
+                
+                var mat = capture.QueryFrame();
+                originalPicture.Image = mat.ToBitmap();
+                await Task.Delay(1000 / Convert.ToInt16(Fps));
+                framesLabel.Text = FrameNo.ToString() + "/" + TotalFrame.ToString();
+                FrameNo += 1;
+                if (capture == null) return;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string videoPath = @"C:\Users\Alex\Documents\GitHub\Laborator_EVA\Star Wars Reflections 4K Unreal Engine Real-Time Ray Tracing Demonstration (360).mp4";
+            string videoPath2 = @"C:\Users\Alex\Documents\GitHub\Laborator_EVA\Do. Or do not. There is no try.mpg";
+            Image output = originalPicture.Image;
+
+            LoadVideo(ref TotalFrame, ref FrameNo, ref Fps, ref capture, ref output, videoPath);
+            originalPicture.Image = output;
+            frameCounter.Minimum = 0;
+            frameCounter.Maximum = TotalFrame;
+            frameCounter.Value = FrameNo;
+            IsReadingFrame = true;
+            ReadAllFrames();
+
+            LoadVideo(ref TotalFrame, ref FrameNo, ref Fps, ref capture, ref output, videoPath2);
+            originalPicture.Image = output;
+            frameCounter.Minimum = 0;
+            frameCounter.Maximum = TotalFrame;
+            frameCounter.Value = FrameNo;
+            IsReadingFrame = true;
+            ReadAllFrames();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            startROI = e.Location;
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (originalPicture.Image == null)
+            {
+                return;
+            }
+            int width = Math.Max(startROI.X, e.X) - Math.Min(startROI.X, e.X);
+            int height = Math.Max(startROI.Y, e.Y) - Math.Min(startROI.Y, e.Y);
+            rectangle = new Rectangle(
+                    Math.Min(startROI.X, e.X),
+                    Math.Min(startROI.Y, e.Y),
+                    width,
+                    height
+                );
+            int mouseX = e.X;
+            int mouseY = e.Y;
+            Refresh();
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+            if (originalPicture.Image == null || rectangle == Rectangle.Empty)
+                return;
+
+            var img = new Bitmap(originalPicture.Image).ToImage<Bgr, byte>();
+            img.ROI = rectangle;
+            var imgROI = img.Copy();
+            modifiedPicture.Image = imgROI.ToBitmap();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (mouseDown)
+            {
+                using (Pen pen = new Pen(Color.Red, 1))
+                {
+                    e.Graphics.DrawRectangle(pen, rectangle);
+                }
+            }
+        }
+    }
+}
